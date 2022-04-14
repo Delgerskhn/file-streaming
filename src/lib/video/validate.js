c76d0a7f49eeea23a2607603704df78947f2baaf
delgerskhn <delgersaikhan2001@gmail.com>
2022-04-14 17:34:10 +0800

    video mime types added

--- src/lib/video/validate.js
+++ src/lib/video/validate.js
@@ -4,6 +4,12 @@ const whitelist = [
   "video/quicktime",
   "video/x-msvideo",
   "video/x-ms-wmv",
+  "video/webm",
+  "video/ogg",
+  "video/x-f4v",
+  "video/x-matroska",
+  "video/MP2T",
+  "video/3gpp",
 ];
 
 const validateVideoType = (req, file, cb) => {
